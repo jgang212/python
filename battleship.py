595b4f9ed8349d472d19e0d97769aa2a460e454d
jgang212 <jack.gang@CHIHQ-IPC160.XRTRADING.LOCAL>
2015-08-04 15:38:33 -0500

    allows for custom rules: # of turns, size of board, # of ships

--- battleship.py
+++ battleship.py
@@ -24,43 +24,87 @@ def random_row(board):
 def random_col(board):
     return randint(0, len(board[0]) - 1)
 
-# create blank board
-board = []
-board_size = 5
+# create a board
+def create_board(board_size):
+    board = []
+        
+    for x in range(board_size):
+        board.append(["O"] * board_size)
+    return board
 
-for x in range(board_size):
-    board.append(["O"] * board_size)
+# get ship coordinates for board
+def get_ship_coordinates(board, numShips):
+    shipCoordinates = []
+    
+    for i in range(0,numShips):
+        shipRow = random_row(board)
+        shipCol = random_col(board)
+        
+        # to make sure that ships don't have identical coordinates
+        if i > 0:
+            while True:
+                if [item for item in shipCoordinates if item[0] == shipRow and item[1] == shipCol]:
+                    shipRow = random_row(board)
+                    shipCol = random_col(board)
+                else:
+                    break
+        shipCoordinates.append((shipRow, shipCol))
+        
+    return shipCoordinates
+    
+###############################################
 
 # game starts
 print "Let's play Battleship!"
+numTurns = positiveIntInput("How many turns do you want? ")
+
+# create blank board
+while True:
+    board_size = positiveIntInput("How big of a square board shall we play with? ")
+    if board_size < 2:
+        print "The board needs to be at least 2x2 to play!"
+    else:
+        break
+board = create_board(board_size)
 print_board(board)
 
-ship1_row = random_row(board)
-ship1_col = random_col(board)
-ship2_row = random_row(board)
-ship2_col = random_col(board)
-while ship1_row == ship2_row and ship1_col == ship2_col:
-    ship2_row = random_row(board)
-    ship2_col = random_col(board)
-
-for turn in range(4):
-    print "Turn", turn + 1
-    
-    guess_row = positiveIntInput("Guess Row:")
-    guess_col = positiveIntInput("Guess Col:")
-    
-    if guess_row == ship1_row and guess_col == ship1_row:
-        print "Congratulations! You sunk my battleship!"
-        break
+# place ships randomly
+while True:
+    numShips = positiveIntInput("How many ships do I have on the board? ")
+    if numShips == 0:
+        print "There needs to be at least one ship to play!"
+    elif numShips > board_size**2:
+        print "There are too many ships for this board!"
     else:
-        if (guess_row < 0 or guess_row > board_size - 1) or (guess_col < 0 or guess_col > board_size - 1):
-            print "Oops, that's not even in the ocean."
-        elif(board[guess_row][guess_col] == "X"):
-            print "You guessed that one already."
-        else:
-            print "You missed my battleship!"
-            board[guess_row][guess_col] = "X"
-        
-        if turn == 3:
-            print "Game Over"
-        print_board(board)
+        break
+shipCoordinates = get_ship_coordinates(board, numShips)
+
+shipsRemaining = numShips
+for turn in range(numTurns):
+    if shipsRemaining == 0:
+        print "Congratulations! You sunk all of my battleships!"
+        break
+    
+    print "-------------------------------"
+    print "Turn", turn + 1    
+    
+    guess_row = positiveIntInput("Guess Row: ")
+    guess_col = positiveIntInput("Guess Col: ")
+    
+    if (guess_row < 0 or guess_row > board_size - 1) or (guess_col < 0 or guess_col > board_size - 1):
+        print "Oops, that's not even in the ocean."
+    elif (board[guess_row][guess_col] == "X" or board[guess_row][guess_col] == "S"):
+        print "You guessed that one already."
+    elif [item for item in shipCoordinates if item[0] == guess_row and item[1] == guess_col]:
+        print "You sunk a battleship!"
+        shipsRemaining = shipsRemaining - 1
+        board[guess_row][guess_col] = "S"
+    else:
+        print "You missed my battleship!"
+        board[guess_row][guess_col] = "X"
+    
+    print "Ships remaining:", shipsRemaining    
+    
+    if turn == numTurns - 1:
+        print "Game Over"
+    print_board(board)
