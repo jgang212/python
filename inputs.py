a522bb7b9870375dc97a01ed5934880b6ca21d2c
jgang212 <jack.gang@CHIHQ-IPC160.XRTRADING.LOCAL>
2015-08-04 14:36:52 -0500

    functions for inputs that check for valid inputs

---
+++ inputs.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 04 14:17:42 2015
+
+@author: jack.gang
+"""
+
+def intInput(string):
+    while True:        
+        fromUser = raw_input(string)
+        try:
+            result = int(fromUser)
+            return result            
+        except ValueError:
+            print "Please enter an integer"
+            
+def positiveIntInput(string):
+    while True:        
+        fromUser = raw_input(string)
+        try:
+            result = int(fromUser)
+            if result >= 0:
+                return result
+            else:
+                print "Please enter a positive integer"
+        except ValueError:
+            print "Please enter a positive integer"
